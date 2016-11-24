866a97e8b77a41f8e8a6bb489c0ee1570c200092
kusuri <mariak@accusonus.com>
2016-11-24 15:06:11 +0200

    Remove JuceLibraryCode from DemoPlugin project

--- DemoPlugin/JuceLibraryCode/modules/juce_opengl/juce_opengl.h
+++
@@ -1,4 +0,0 @@
-// This is an auto-generated file to redirect any included
-// module headers to the correct external folder.
-
-#include "../../../../../../SDKs/JuceLibrary/4.1.0/modules/juce_opengl/juce_opengl.h"
